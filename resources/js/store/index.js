31ced91e85e181dc8bc047be647f64532e38230d
0102763801elmansey <almanseyalmansey@gmail.com>
2021-12-02 12:12:49 +0200

    chnge in store file

--- resources/js/store/index.js
+++ resources/js/store/index.js
@@ -4,7 +4,7 @@ import router from '../router'
 // import 'es6-promise/auto';
 import layout from './modules/layout'
 import menu from './modules/menu'
-import auth from './modules/Auth'
+import auth from './modules/auth'
 import role from './modules/role'
 import admin from './modules/admin'
 import setting from './modules/setting'
